625975371dd72d7ead382f1532b2fc0914520a4b
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-03-07 23:13:53 +0200

    Add files via upload

---
+++ number_samples.py
@@ -0,0 +1,23 @@
+x1 = -4.556
+x2 = 4455
+y = abs(x1)
+y = abs(x2)
+x = 3.5654323244556643433
+y = round(x,2)
+z = float(x2)
+f = bin(x2)
+g = hex(x2)
+h = oct(x2)
+z = str(x2)
+#a = 'AAB','A','BB','SS','A','EE','BN','BW','A','A'
+b = max('23','112','44','22','1','22','321','1','1','15')
+
+a='11122AAAASS'
+
+print(a.isalpha(), a.isnumeric())
+
+import datetime as dt
+
+today = dt.date.today()
+day = dt.date.today().day
+print(today, day)
